bad379491f83420b2d084aaf3f51f0b121fb033e
maorgigi123 <maorgigim1509@gmail.com>
2024-12-17 20:03:19 +0200

    add error handling

--- server.ts
+++ server.ts
@@ -22,24 +22,26 @@ const app = express();
 // Connect to MongoDB
 mongoose.connect(mongoURI)
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
-
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // Ensures the app exits if MongoDB connection fails
+  });
 // Enable CORS
 app.use(cors());
 app.use(helmet());  // מוסיף הגנה למגוון בעיות אבטחה
 
-// invoke the GetCurrentPage function when route is images
+// Invoke the GetCurrentPage function when route is /images
 app.get('/images', async (req: Request, res: Response) => {
   try {
     const getCurrentPageHandler = GetCurrentPage(Categories, DataByCategories);
     // Directly invoke the handler
     await getCurrentPageHandler(req, res);
   } catch (err) {
-   console.log('error')
+    // Log error details for debugging purposes
+    console.error('Error occurred:', err);
+    res.status(500).json({ error: 'An error occurred while fetching data' });
   }
 });
-
-
 
 // Start the server
 app.listen(process.env.PORT || 3001, () => {
